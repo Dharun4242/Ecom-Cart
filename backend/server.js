967f43a78608d317899fdfa9d583b51af9ff7eaa
Dharun4242 <dharunjayakrishnan@gmail.com>
2025-11-07 18:41:30 +0530

    Initial commit - clean setup

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const MONGO_URI = process.env.MONGO_URI;
+
+
+app.use(cors());
+app.use(express.json()); 
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(MONGO_URI);
+        console.log('MongoDB Connected successfully!');
+    } catch (err) {
+        console.error('MongoDB connection error:', err.message);
+        process.exit(1); 
+    }
+};
+
+connectDB();
+
+app.get('/', (req, res) => {
+    res.send('API is running...');
+});
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
